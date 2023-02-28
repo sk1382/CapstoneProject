c98010df95201fca9fe1f7fbe3674b7617dd0d64
sri <chichova1382@gmail.com>
2023-02-28 16:48:37 +0530

    Random Commit

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from subtraction import subtract
+from addition import add
 
 while True:
     choice = input("1. Add\n2. Subtract\n3. Exit\nEnter you choice:")
@@ -8,7 +9,9 @@ while True:
         print("Thanks for using our program!")
         break
     elif choice == "1":
-        pass
+        print(add(num1, num2))
     elif choice == "2":
         print(subtract(num1, num2))
+    else:
+        print()
 
