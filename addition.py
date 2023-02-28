17d24afd9169a6d9dfac8d69871cd7c94915ecd5
sri <chichova1382@gmail.com>
2023-02-28 15:21:56 +0530

    Added addition.py

--- addition.py
+++ addition.py
@@ -1,10 +1,3 @@
 def add(x, y):
     return x + y
-
-try:
-    num1 = float(input("Enter first number: "))
-    num2 = float(input("Enter second number: "))
-except ValueError:
-    print("Invalid input. Please enter a number.")
-
-print(num1, "+", num2, "=", add(num1, num2))
+    
